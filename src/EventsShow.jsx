f43bc33f115b056b4f431b581a8751a83a76326d
f-pinkus <faigypinkus@gmail.com>
2025-06-29 16:20:46 -0400

    list attendees in event show action

--- src/EventsShow.jsx
+++ src/EventsShow.jsx
@@ -5,6 +5,15 @@ export function EventsShow({ event }) {
       <h3>{event.name}</h3>
       <h5>{event.date}</h5>
       <h5>{event.location}</h5>
+
+      <h3>Attendees:</h3>
+      <ul>
+        {event.attendees.map((attendee) => (
+          <li key={attendee.id}>
+            {attendee.name} ({attendee.email})
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
